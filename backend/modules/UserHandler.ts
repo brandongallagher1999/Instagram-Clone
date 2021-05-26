2645ad0acc5bfae27d9ee22c9877954b8259a6f8
Brandon Gallagher <brandonegallagher@gmail.com>
2021-05-26 17:13:02 -0400

    Almost done the registration and login aspect.

--- backend/modules/UserHandler.ts
+++ backend/modules/UserHandler.ts
@@ -4,6 +4,8 @@
 import mongoose, { Collection, Error, model, Model } from "mongoose";
 import { IUser } from "../types/user";
 import UserSchema from "../models/User";
+//@ts-ignore
+import jhashes from "jshashes";
 
 import DatabaseHandler from "./DatabaseHandler";
 
@@ -11,26 +13,45 @@ import DatabaseHandler from "./DatabaseHandler";
 class UserHandler extends DatabaseHandler
 {
 
-    UserCollection: Model<IUser>;
+    UserModel: Model<IUser>;
     constructor()
     {
         super();
-        this.UserCollection = model("User", UserSchema); // Connection is somehow handled by mongoose?
+        this.UserModel = model("User", UserSchema); // Connection is somehow handled by mongoose?
 
     }
 
-    login(user: IUser)
+    register(user: IUser): number
     {
-        this.UserCollection.findOne({"username" : user.username}, (err: Error, userObj: IUser) => {
-            if (err)
+        let statusCode = 401;
+        user.password = jhashes.SHA1(user.password); //hash the password
+        this.UserModel.findOne({"username" : user.username}, (err: Error, userObj: IUser) => {
+            if (!userObj) //if the user doesn't exist.
             {
-                return 400;
+                statusCode = 200;
+                const newUser = new this.UserModel(user);
+                newUser.save();
             }
             else
             {
-                
+                statusCode = 401;
             }
         });
+        return statusCode;
+        
+    }
+
+    login(user: IUser): number
+    {
+        let statusCode = 200;
+        const hashedPassword: string = jhashes.SHA1(user.password); //hashing the password
+        this.UserModel.findOne({"username" : user.username, "password" : hashedPassword}, (err: Error, userObj: IUser) => {
+            if (err)
+            {
+                statusCode = 404;
+            }
+        });
+        return statusCode;
     }
     
     
---
+++ backend/routers/UserRoute.ts
@@ -0,0 +1,45 @@
+import { Router, Response, Request } from "express";
+import { IUser } from "../types/user";
+import UserHandler from "../modules/UserHandler";
+import { jwt_secret } from "../config.json";
+import cookie from "cookie";
+
+
+export {};
+
+import jwt from "jsonwebtoken";
+
+const express: any = require("express");
+
+const router: Router = express.Router();
+
+const userHandler: UserHandler = new UserHandler();
+
+//POST: /api/login/
+router.post("/login/", (req: Request, res: Response, next: any): any => {
+
+    const user: IUser = req.body;
+    if (userHandler.login(user) == 200)
+    {
+        const json_token = jwt.sign({ data: {"username" : user.username}}, jwt_secret, { expiresIn: "2h"});
+        res.setHeader("Set-Cookie", cookie.serialize("jwt", json_token, {httpOnly: true}));
+        res.status(200).send("Success");
+    }
+    else
+    {
+        res.status(404).send("Login failed. User not found.");
+    }
+    next();
+});
+
+router.post("/register", (req: Request, res: Response, next: any): any => {
+    const user: IUser = req.body;
+
+    userHandler.register(user)
+    next();
+
+});
+
+
+
+
